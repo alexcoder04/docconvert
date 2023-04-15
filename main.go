0f18bc1ddf52a950d5ee1472ac94363d8a9ca86d
alexcoder04 <alexcoder04@protonmail.com>
2023-04-15 17:33:06 +0200

    embed template into binary

--- main.go
+++ main.go
@@ -1,32 +1,57 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
-	"net/http"
+	"text/template"
 
 	"github.com/alexcoder04/docconvert/handlers"
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed templates/index.html
+var TemplateFiles embed.FS
+
 var (
+	Template *template.Template
+
 	ApiOnly  = flag.Bool("api-only", false, "run the program in API-only mode")
 	AllHosts = flag.Bool("all-hosts", false, "listen on 0.0.0.0")
 	Port     = flag.Int("port", 8080, "port number to listen on")
 )
 
+func LoadTemplate() (*template.Template, error) {
+	tmpl := template.New("")
+
+	contents, err := TemplateFiles.ReadFile("templates/index.html")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = tmpl.Parse(string(contents))
+	if err != nil {
+		return nil, err
+	}
+
+	return tmpl, nil
+}
+
 func main() {
 	flag.Parse()
 
 	gin.SetMode(gin.ReleaseMode)
-
 	router := gin.Default()
 
 	if !*ApiOnly {
-		router.LoadHTMLGlob("templates/*.html")
+		var err error
+		Template, err = LoadTemplate()
+		if err != nil {
+			panic("failed to load html template")
+		}
 
 		router.GET("/", func(c *gin.Context) {
-			c.HTML(http.StatusOK, "index.html", gin.H{})
+			Template.Execute(c.Writer, gin.H{})
 		})
 	}
 
