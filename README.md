0d9a0540e774b1a44403499488be6859ba277a21
alexcoder04 <alexcoder04@protonmail.com>
2023-04-15 17:03:23 +0200

    add port conf option and docs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+
 # docconvert
-Very simple website for converting documents from/to different formats
+
+Very simple website for converting documents from/to different formats.
+
+## Build and run
+
+```sh
+git clone https://github.com/alexcoder04/docconvert
+cd docconvert
+go build
+./docconvert
+```
+
+## Command-line arguments
+
+ - `-port`: port to run on, default 8080
+ - `-api-only`: run in api-only mode, without web gui
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/alexcoder04/docconvert/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-var ApiOnly = flag.Bool("api-only", false, "run the program in API-only mode")
+var (
+	ApiOnly = flag.Bool("api-only", false, "run the program in API-only mode")
+	Port    = flag.Int("port", 8080, "port number to listen on")
+)
 
 func main() {
 	flag.Parse()
@@ -26,7 +30,5 @@ func main() {
 	router.POST("/convert/upload", handlers.HandleFileUpload)
 	router.POST("/convert/text", handlers.HandleTextData)
 
-	if err := router.Run(":8080"); err != nil {
-		panic(err)
-	}
+	router.Run(fmt.Sprintf(":%d", *Port))
 }
